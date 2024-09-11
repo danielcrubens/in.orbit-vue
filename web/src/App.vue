4217392043ca519d61961b0100345523edbf71fd
danielcrubens <dcr.tecnologi@gmail.com>
2024-09-11 15:32:22 -0300

    feat: estuturando components  :white_check_mark:

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <DialogRoot>
+    <div class="h-screen flex flex-col items-center justify-center gap-8">
+      <EmptyGoals />
+      <DialogOverlay class="fixed inset-0 z-[99] bg-black bg-opacity-10 backdrop-blur-sm" />
+      <CreateGoal />
+    </div>
+  </DialogRoot>
 </template>
+
+<script setup>
+import CreateGoal from "./components/ui/CreateGoal/CreateGoal.vue";
+import Button from "./components/ui/Button.vue";
+import EmptyGoals from "./components/EmptyGoals.vue";
+import { DialogOverlay, DialogRoot } from "radix-vue";
+import { ref } from 'vue';
+const radioStateSingle = ref('default');
+</script>
